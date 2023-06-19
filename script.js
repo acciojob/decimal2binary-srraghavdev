9248ad63e2eee659001901a9a4f8b1a42b327aab
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-19 18:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function threeSum(arr, target) {
 // write your code here
 	let str=""
 	 while(arr>0){
-		 str+=(arr%10)
+		 str+=(arr%2)
 		 arr=Math.floor(arr/10)
 				}
 		target= parseInt(str.reverse())
